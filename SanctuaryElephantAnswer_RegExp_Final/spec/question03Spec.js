713879f77bb2297a5dbce2ffdfb9c01b1c2f4965
SumiPrasad <30812898+SumiPrasad@users.noreply.github.com>
2017-09-11 18:11:50 +1200

    Spec 3 and Spec 7 edited with RegExp

---
+++ SanctuaryElephantAnswer_RegExp_Final/spec/question03Spec.js
@@ -0,0 +1,50 @@
+/* globals describe beforeEach  it expect Controller Sanctuary */
+describe('Question Three', () => {
+  describe('Sanctuary.getElephantOwners function', () => {
+    let theSanctuary
+    beforeEach(() => {
+      theSanctuary = Controller.setup()
+    })
+
+    it('should return a string', () => {
+      expect(typeof theSanctuary.getElephantOwners()).toBe('string')
+    })
+
+    it('should NOT be hard coded', () => {
+      theSanctuary = new Sanctuary()
+      expect(theSanctuary.getElephantOwners()).toBe('')
+    })
+
+ //'Adam, Taylor. [ATR]\nJoby, Thomas. [JOT]\nRobert, William. [RWM]\nTania, Paul. [TPL]\n')
+    
+	describe('Should return correctly formatted data as below', () => {
+	
+	it('Subpart 1 : First character of First name should be in [A,J,R,T]', () => {
+	 expect(theSanctuary.getElephantOwners()).toMatch(/^[AJRT](.{3,5})/)
+	  })
+	
+	it('Subpart 2: First name should be followed by a comma and space', () => {
+      expect(theSanctuary.getElephantOwners()).toMatch(/[,\s)]/)
+	  })
+	 
+	  it('Subpart 3: Last character of Second name should be in [r,s,m,l]', () => {
+	  let anElephantOwner
+      anElephantOwner = theSanctuary.getElephantOwners()
+	  anElephantOwner = theSanctuary.allMyElephantOwners[0,1,2,3]
+	  expect(anElephantOwner.lastName).toMatch(/^[TWP](.{3,6})/)
+	  })
+	
+	it('Subpart 4: Second name should be followed by a dot and space', () => {
+      expect(theSanctuary.getElephantOwners()).toMatch(/[\.\s]/)
+	  })
+	  
+      it('Subpart 5: should have three character id enclosed in []', () => {
+      expect(theSanctuary.getElephantOwners()).toMatch(/\[\w{3}\]/)
+	  })
+	  
+	  it('Subpart 6: should have a newline', () => {
+      expect(theSanctuary.getElephantOwners()).toMatch(/\n/)
+	  })
+	})
+  })
+})
